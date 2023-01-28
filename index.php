d8fb5b5809a78fa9c01e0ac6b9b08e74261fc242
Tuna <df95111111@gmail.com>
2023-01-29 07:33:42 +0800

    feat: 加入google 分析的嵌入碼

--- index.php
+++ index.php
@@ -38,6 +38,17 @@ $answer = $answerArr[$_GET['animal']]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1ZH5RN8HBS"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-1ZH5RN8HBS');
+    </script>
 </head>
 
 <body>
@@ -54,6 +65,7 @@ $answer = $answerArr[$_GET['animal']]; ?>
             </div>
         </div>
     </div>
+
 </body>
 
 </html>
